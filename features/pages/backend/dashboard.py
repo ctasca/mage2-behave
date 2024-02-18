7d339602aec1b47f838b08e646288a7648c79375
Carlo Tasca <c.tasca.1971@gmail.com>
2024-02-18 11:37:40 +0100

    Add dashboard page with admin menu

---
+++ features/pages/backend/dashboard.py
@@ -0,0 +1,7 @@
+from stere import Page
+from .areas.admin_menu import AdminMenu
+
+class Dashboard(Page):
+    def __init__(self) -> None:
+        super().__init__()
+        self.admin_menu = AdminMenu()
