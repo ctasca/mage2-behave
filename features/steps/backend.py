ef84fab2d997a9770931edadc202b22ae7199f5c
Carlo Tasca <c.tasca.1971@gmail.com>
2024-02-18 11:39:21 +0100

    Add backend and developer tests steps

---
+++ features/steps/backend.py
@@ -0,0 +1,71 @@
+import core_config.all as config
+from behave import *
+from pages.backend.login import Login
+from pages.backend.dashboard import Dashboard
+
+@given(u'I am on the admin login page')
+def step_impl(context):
+    with Login() as page:
+        page.base_url = context.baseurl
+        page.url_suffix = context.backend + 'login'
+        page.navigate()
+
+@when(u'I enter my credentials')
+def step_impl(context):
+    with Login() as page:
+        page.form.username.fill(context.admin_username)
+        page.form.password.fill(context.admin_password)
+
+@when(u'I sign in')
+def step_impl(context):
+    with Login() as page:
+        page.form.signin.click()
+
+@then(u'I should see a dashboard header')
+def step_impl(context):
+    assert context.browser.is_text_present("Dashboard") is True
+
+@given(u'I am logged in the backend')
+def step_impl(context):
+    open_login_page = 'Given I am on the admin login page'
+    fill_fields = 'When I enter my credentials'
+    signin = 'And I sign in'
+    context.execute_steps(f'''
+        {open_login_page}
+        {fill_fields}
+        {signin}
+    ''')
+
+@when(u'I click the "{item}" menu item')
+def step_impl(context, item):
+    with Dashboard() as page:
+        if item == 'Sales':
+            page.admin_menu.sales.click()
+        if item == 'Catalog':
+            page.admin_menu.catalog.click()
+        if item == 'Customers':
+            page.admin_menu.customers.click()
+
+@then(u'I should see the "{item}" submenu links')
+def step_impl(context, item):
+    with Dashboard() as page:
+        if item == "Sales":
+            assert page.admin_menu.submenus['sales'].orders.is_visible() is True
+            assert page.admin_menu.submenus['sales'].invoices.is_visible() is True
+            assert page.admin_menu.submenus['sales'].shipments.is_visible() is True
+            assert page.admin_menu.submenus['sales'].creditmemos.is_visible() is True
+            assert page.admin_menu.submenus['sales'].billing_agreements.is_visible() is True
+            assert page.admin_menu.submenus['sales'].transactions.is_visible() is True
+            assert page.admin_menu.submenus['sales'].virtual_terminal.is_visible() is True
+
+        if item == 'Catalog':
+            assert page.admin_menu.submenus['catalog'].products.is_visible() is True
+            assert page.admin_menu.submenus['catalog'].categories.is_visible() is True
+        
+        if item == 'Customers':
+            assert page.admin_menu.submenus['customers'].all_customers.is_visible() is True
+            assert page.admin_menu.submenus['customers'].now_online.is_visible() is True
+            assert page.admin_menu.submenus['customers'].login_as_customer_log.is_visible() is True
+            assert page.admin_menu.submenus['customers'].customer_groups.is_visible() is True
+
+
---
+++ features/steps/dev_tests.py
@@ -0,0 +1,13 @@
+from behave import *
+from pages.google import Google
+
+@given(u'I visit "https://www.google.com/"')
+def step_impl(context):
+   with Google() as page:
+        page.base_url = "https://www.google.com/"
+        page.navigate()
+
+
+@then(u'the title should be "{title}"')
+def step_impl(context, title):
+    assert title in context.browser.title
