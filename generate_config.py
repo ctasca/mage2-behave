df6912d230a13799ed132309570c6c2b50a6be2e
Carlo Tasca <c.tasca.1971@gmail.com>
2024-02-27 11:26:56 +0100

    Improve generate_config script/add .ini templates

--- generate_config.py
+++ generate_config.py
@@ -9,6 +9,7 @@ dest_dir = 'features/core_config'
 
 # Get list of .dist files
 dist_files = [f for f in os.listdir(source_dir) if f.endswith('.dist')]
+template_files = [f for f in os.listdir(source_dir) if f.endswith('.template')]
 
 # Create destination directory if it doesn't exist
 if not os.path.exists(dest_dir):
@@ -18,11 +19,16 @@ if not os.path.exists(dest_dir):
 for dist_file in dist_files:
     src_file = os.path.join(source_dir, dist_file)
     dest_file_name = os.path.splitext(dist_file)[0] + '.ini'
+    template_file = os.path.join(source_dir, '.{}.template'.format(dest_file_name))
     dest_file = os.path.join(dest_dir, dest_file_name)
 
     # If the file doesn't exist in destination directory, copy it
+    # from template file
     if not os.path.exists(dest_file):
-        shutil.copy2(src_file, dest_file)
+        print(f'{colorama.Fore.GREEN}Copying {template_file} to {dest_file}{colorama.Style.RESET_ALL}')
+        print(f'{colorama.Fore.RED} Re-run this script to be able to set the parameters in {dest_file}'
+              f'{colorama.Style.RESET_ALL}')
+        shutil.copy2(template_file, dest_file)
     else:
         # If the file does exist, merge variables
 
@@ -33,21 +39,35 @@ for dist_file in dist_files:
         # Parse destination config file
         dest_config = configparser.ConfigParser()
         dest_config.read(dest_file)
-
+        skip_all = False
         # Iterate over each variable in source config file
         for section in src_config.sections():
+            if skip_all is True:
+                break
+            print(f'{colorama.Fore.WHITE}Configuring section {colorama.Fore.YELLOW}{section}{colorama.Style.RESET_ALL} '
+                  f'{colorama.Fore.WHITE}in {dest_file} {colorama.Style.RESET_ALL}')
             for key in src_config[section]:
-                # If the variable isn't in the destination config file, prompt the user to set it
+                if dest_config.has_option(section, key):
+                    print(f'{colorama.Fore.LIGHTCYAN_EX}{key}{colorama.Style.RESET_ALL} '
+                          f'{colorama.Fore.GREEN}has already a value in {dest_file} section {section}'
+                          f'{colorama.Style.RESET_ALL}'
+                          f'{colorama.Fore.RED} ...skipping{colorama.Style.RESET_ALL}')
+                    continue
                 if not dest_config.has_option(section, key):
                     print(f'{colorama.Fore.YELLOW}Variable '
                           f'{colorama.Fore.LIGHTCYAN_EX}{key}{colorama.Style.RESET_ALL}{colorama.Fore.YELLOW} '
                           f'in section {section} is not set.{colorama.Style.RESET_ALL}')
                     value = input(f'>>> {colorama.Fore.LIGHTBLUE_EX}Please provide a value, '
-                                  f'or press enter to skip: {colorama.Style.RESET_ALL}')
+                                  f'press enter to skip to next value,'
+                                  f' or type {colorama.Fore.RED}skip{colorama.Style.RESET_ALL} '
+                                  f'{colorama.Fore.LIGHTBLUE_EX}to jump to the next configuration file: '
+                                  f'{colorama.Style.RESET_ALL}')
+                    if value == 'skip':
+                        skip_all = True
+                        break
                     if value is None or value.strip() == '':
                         continue
                     dest_config.set(section, key, value)
-
         # Write updated config to destination file
         with open(dest_file, 'w') as configfile:
             dest_config.write(configfile)
