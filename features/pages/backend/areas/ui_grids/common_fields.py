9b03a32a91be4d4b88260f09b86cb511f15013d3
Carlo Tasca <c.tasca.1971@gmail.com>
2024-03-01 11:40:26 +0100

    Remove unneccessary inheritance

--- features/pages/backend/areas/ui_grids/common_fields.py
+++ features/pages/backend/areas/ui_grids/common_fields.py
@@ -4,7 +4,6 @@ from features.pages.backend.fields.grid_fulltext_search_input import GridFulltex
 from features.pages.backend.fields.grid_fulltext_search_button import GridFulltextSearchButton
 from features.pages.backend.fields.grid_apply_filters_button import GridApplyFiltersButton
 from features.pages.backend.fields.grid_clear_all_filters_button import GridActiveFiltersClearAllButton
-from stere.areas import Area
 from stere.fields import Button, Input
 
 
@@ -34,25 +33,11 @@ class UiGridFiltersInterface(ABC):
         pass
 
 
-class GridCommonFields(Area):
-    FILTERS_BUTTON: str = 'filters_button'
-    FULLTEXT_SEARCH_INPUT: str = 'fulltext_search_input'
-    FULLTEXT_SEARCH_BUTTON: str = 'fulltext_search_button'
-    APPLY_FILTERS_BUTTON: str = 'apply_filters_button'
-    CLEAR_ALL_FILTERS_BUTTON: str = 'clear_all_filters_button'
-
+class GridCommonFields:
     def __init__(self):
-        super().__init__()
         self.filters_button = GridFiltersButton()
         self.fulltext_search_input = GridFulltextSearchInput()
         self.fulltext_search_button = GridFulltextSearchButton()
         self.apply_filters_button = GridApplyFiltersButton()
         self.clear_all_filters_button = GridActiveFiltersClearAllButton()
 
-    def get_button(self, button_name: str) -> Button:
-        """ Use the constants defined in this class to retrieve a button"""
-        return getattr(self, button_name)
-
-    def get_input(self, input_name: str) -> Input:
-        """ Use the constants defined in this class to retrieve an input"""
-        return getattr(self, input_name)
