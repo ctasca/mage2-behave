c7bd52a51ae94f35a99460c2ccb6dcf410d6e5a4
Carlo Tasca <c.tasca.1971@gmail.com>
2024-03-16 11:53:19 +0100

    Add bck dir to safely store backup of env.php

--- utils/behave_m2_env.py
+++ utils/behave_m2_env.py
@@ -6,11 +6,13 @@ from colorama import Fore, Style
 from .confirmation import yes_no_input
 
 script_directory = os.path.dirname(__file__)
-directory_path = os.path.join(script_directory, '..', '..', 'mage2-behave')
-absolute_path = os.path.abspath(directory_path)
+mage2_directory = os.path.join(script_directory, '..', '..', 'mage2-behave')
+bck_path = os.path.join(script_directory, '..', 'bck')
+absolute_path = os.path.abspath(mage2_directory)
 parent_directory = os.path.dirname(absolute_path)
 app_etc_directory = os.path.join(parent_directory, os.path.join('app', 'etc'))
 env_file = os.path.join(app_etc_directory, 'env.php')
+env_bck_file = os.path.join(bck_path, 'env.php.bck')
 env_orig_file = os.path.join(app_etc_directory, 'env.php.orig.behave')
 behave_flag = os.path.join(app_etc_directory, 'behave.flag')
 
@@ -20,6 +22,7 @@ def copy_and_modify_env_db_data(context):
         raise Exception('env.php file could not be found!')
 
     shutil.copy(env_file, env_orig_file)
+    shutil.copy(env_file, env_bck_file)
 
     with open(behave_flag, 'w') as file:
         file.write("# Behave flag file")
@@ -37,7 +40,8 @@ def copy_and_modify_env_db_data(context):
 
 def reset_env_db_data():
     shutil.copy(env_orig_file, env_file)
-    if os.path.exists(behave_flag) and os.path.isfile(behave_flag):
+    if ((os.path.exists(behave_flag) and os.path.isfile(behave_flag)) or
+            (os.path.exists(env_orig_file) and os.path.isfile(env_orig_file))):
         os.remove(env_orig_file)
         os.remove(behave_flag)
 
