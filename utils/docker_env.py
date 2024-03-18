f37345d6606aaa34dd051d433796781b1528bb0e
Carlo Tasca <c.tasca.1971@gmail.com>
2024-03-18 15:47:57 +0100

    improve regex for docker container search

--- utils/docker_env.py
+++ utils/docker_env.py
@@ -12,7 +12,7 @@ def docker_client_from_env() -> DockerClient:
 
 def container_id_search(search: str) -> str:
     """ Performs a search to find the container ID from the environment """
-    pattern = re.compile(rf".*{search}(?!-debug).*")
+    pattern = _container_search_pattern(search)
     docker_client = docker_client_from_env()
     containers = docker_client.containers.list()
     for container in containers:
@@ -23,7 +23,7 @@ def container_id_search(search: str) -> str:
 
 def container_name_search(search: str) -> str:
     """ Performs a search to find the container name from the environment """
-    pattern = re.compile(rf".*{search}(?!-debug).*")
+    pattern = _container_search_pattern(search)
     docker_client = docker_client_from_env()
     containers = docker_client.containers.list()
     for container in containers:
@@ -41,3 +41,7 @@ def docker_bin_magento(command: str):
     process = subprocess.Popen(full_command, shell=True)
     process.wait()
     return
+
+
+def _container_search_pattern(search: str) -> re.Pattern:
+    return re.compile(rf".*{search}(?!_|-(.debug)).*")
